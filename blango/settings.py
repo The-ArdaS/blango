94fa03a71369d370e72211c98f1fa0778b9d41f0
Sadra TZ <sadra_alex13@yahoo.com>
2022-10-12 13:10:11 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 import os
+from datetime import timedelta
 from pathlib import Path
 from configurations import Configuration, values
 import dj_database_url
@@ -194,6 +194,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
 
         "DEFAULT_PERMISSION_CLASSES": [
@@ -235,7 +236,10 @@ class Dev(Configuration):
         }
     }
 
-    
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
 
 class Prod(Dev):
